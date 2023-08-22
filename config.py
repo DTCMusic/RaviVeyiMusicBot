d2acc4501974798b4dfe0c3c9cb2bfd5e27252eb
PornoHup <107538335+PornoHup@users.noreply.github.com>
2023-08-23 01:05:31 +0400

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("16102648"))
+API_HASH = getenv("378a73e340eb634cf67c8c42bafa9f37")
 
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
@@ -16,7 +16,7 @@ OWNER_ID = int(getenv("OWNER_ID"))
 PING_IMG = getenv("PING_IMG", "https://images.app.goo.gl/LbhfP48RAK1mHKvn7")
 START_IMG = getenv("START_IMG", "https://images.app.goo.gl/azovyQQZ44g2s5TW8")
 
-SESSION = getenv("SESSION", None)
+SESSION = getenv("SESSION", AgBBAwGPjEkCV-WUNsXHJQfv_SeroXM9zXbdhVrfqnqDRVYN5RbZY6zvgmafx7908wpKv8j-EIyF1B0YXMzcJVn7yn57g15PtDu3q8scfCCXzwyqCJZs2NEZnzjm0HmVvQKau92TLH_i-ueN6PvqlGFEcMrNOhyosaohie52pmLFBQpsy0ihjdoMbjFqBRuy6LJ9u2AjRy6KkaphKXedBJGHTbrADUZA1PFB8qJ3govOpRBsJxSETTDXUok1QwKFgV4wQ4_yN61kzObHUYxUfFoDfeE0zvYTTvi5NmS_ptUPubYJibfFHWVJFzuMMH4EYJjgs2uW-IepGP2ujEboTRd4AAAAAUiRuNYA)
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/riyaddSupport")
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AyselProje")
